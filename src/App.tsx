4f55c3a59e5b15b7a7b0a3717f3ee1e6761a18c4
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-06 17:30:36 +0300

    feat: add accardion to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { Accardion } from './components/accardion/Accardion';
 import { Rating } from './components/rating/Rating';
 
 function App() {
   return (
     <div className="App">
       <Rating/>
+      <Accardion/>
     </div>
   );
 }
