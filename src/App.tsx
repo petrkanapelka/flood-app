8728726a2e245910e1ad5af1651d3a9fb30b2213
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-12 23:02:52 +0300

    feat: add new copmponents to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
+import { useState } from 'react';
 import './App.css';
-import { Accardion } from './components/accardion/Accardion';
-import { Rating } from './components/rating/Rating';
+import { ControlledAccardion } from './components/controlledAccardion/ControlledAccardion';
+import { UncontrolledAccardion } from './components/uncontrolledAccardion/UncontrolledAccardion';
+import { ControlRating } from './components/contolRating/ControlRating';
+import { UncontrolRating } from './components/uncontrolRating/UncontrolRating';
+
+export type itemType = {
+  id: number,
+  name: string
+}
+
+const items: Array<itemType> = [
+  { id: 1, name: 'CSS' },
+  { id: 2, name: 'JS' },
+  { id: 3, name: 'TS' },
+  { id: 4, name: 'React' },
+  { id: 5, name: 'HTML' },
+]
+
+
+const Stars: Array<{value:number}> = [
+  { value: 1 },
+  { value: 2 },
+  { value: 3 },
+  { value: 4 },
+  { value: 5 },
+];
 
 function App() {
+
+  let [collapsed, setCollapsed] = useState(false);
+
+  const [ratingValue, setRatingValue] = useState(0);
+
   return (
     <div className="App">
-      <Rating/>
-      <Accardion/>
+      <UncontrolledAccardion setCollapsed={setCollapsed} collapsed={collapsed} items={items} />
+      <UncontrolRating setRatingValue={setRatingValue} ratingValue={ratingValue} stars={Stars}/>
+      <ControlRating />
+      <ControlledAccardion />
     </div>
   );
 }
