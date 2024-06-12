d30a83fb1ced03f6afc472844789f93bb64461cb
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-12 23:01:46 +0300

    feat: create ControlAccardion

---
+++ src/components/contolRating/ControlRating.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const Stars = [
+    { value: 1 },
+    { value: 2 },
+    { value: 3 },
+    { value: 4 },
+    { value: 5 },
+];
+
+export const ControlRating = () => {
+
+    const [ratingValue, setRatingValue] = useState(0);
+
+    const handleClick = (starValue: number) => {
+        if (starValue === ratingValue) {
+            setRatingValue(0);
+        } else {
+            setRatingValue(starValue);
+        }
+    };
+
+    const StarsDivs = Stars.map((star, indx) => {
+        const isSelected = indx + 1 <= ratingValue;
+        return (
+            <div
+                key={star.value}
+                className={`star ${isSelected ? 'star-selected' : ''}`}
+                onClick={() => handleClick(star.value)}
+            ></div>
+        );
+    });
+
+    return <div style={{ display: 'flex', gap: '15px' }}>
+        <h3>Uncontrolled rating</h3>
+        {StarsDivs}
+    </div>;
+};
+
