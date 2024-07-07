534bd96050188a9fe066fe65857c4747c67115f5
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-07 21:24:41 +0300

    input stories

--- src/components/input/Input.stories.tsx
+++ src/components/input/Input.stories.tsx
@@ -1,4 +1,6 @@
 import { ChangeEvent, useRef, useState } from "react";
+import { action } from '@storybook/addon-actions'
+
 
 export default {
     title: 'input'
@@ -30,4 +32,38 @@ export const GetValueUncontrolledInput = () => {
         </>
     )
 }
-export const ControlledInput = () => <input value={'it-incubator'} />
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('');
+    return (
+        <input value={parentValue} onChange={(e) => {
+            // action('want to change');
+            setParentValue(e.currentTarget.value)
+        }} />
+    )
+}
+
+export const ControlledCheckBox = () => {
+    const [parentValue, setParentValue] = useState(true);
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+
+    }
+    return (
+        <input type='checkbox' checked={parentValue} onChange={onChange} />
+    )
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined);
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return (
+        <select value={parentValue} onChange={onChange}>
+            <option >None</option>
+            <option value={'1'}>Minsk</option>
+            <option value={'2'}>Kiev</option>
+            <option value={'3'}>Moscow</option>
+        </select>
+    )
+}
