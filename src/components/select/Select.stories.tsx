9fe9b0022cb9fb5e83650464f00e3821d2c73966
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-07 23:47:47 +0300

    storybook: add Select story

---
+++ src/components/select/Select.stories.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { Select } from "./Select";
+import { action } from '@storybook/addon-actions'
+
+
+export default {
+    title: "Select",
+    component: 'Select',
+};
+
+
+export const WithValue = () => {
+    const [value, setValue] = useState('2')
+    return (
+        <>
+            <Select onChange={setValue}
+                value={value}
+                items={
+                    [
+                        { value: '1', title: 'Minsk' },
+                        { value: '2', title: 'Moscow' },
+                        { value: '3', title: 'Kiev' }
+                    ]
+                }
+            />
+        </>
+    )
+}
+
+export const WithOutValue = () => {
+    const [value, setValue] = useState(null)
+
+    return (
+        <>
+            <Select onChange={setValue}
+                value={value}
+                items={
+                    [
+                        { value: '1', title: 'Minsk' },
+                        { value: '2', title: 'Moscow' },
+                        { value: '3', title: 'Kiev' }
+                    ]
+                }
+            />
+        </>
+    )
+}
