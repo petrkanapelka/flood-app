42aaeb84e690d08f68c4beb33597539a8d521ec2
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-08 22:11:53 +0300

    feat: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { ControlledAccardion } from './components/controlledAccardion/ControlledAccardion';
 import { UncontrolledAccardion } from './components/uncontrolledAccardion/UncontrolledAccardion';
 import { ControlRating } from './components/contolRating/ControlRating';
 import { UncontrolRating } from './components/uncontrolRating/UncontrolRating';
+import { itemsSelected, Select } from './components/select/Select';
 
 export type itemType = {
   id: number,
@@ -19,7 +20,7 @@ export const items: Array<itemType> = [
 ]
 
 
-export const Stars: Array<{value:number}> = [
+export const Stars: Array<{ value: number }> = [
   { value: 1 },
   { value: 2 },
   { value: 3 },
@@ -33,12 +34,21 @@ function App() {
 
   const [ratingValue, setRatingValue] = useState(0);
 
+  const [value, setValue] = useState('2')
+
+  const UncontrolRatingMemo = React.memo(UncontrolRating)
+  const ControlRatingMemo = React.memo(ControlRating)
+  const UncontrolledAccardionMemo = React.memo(UncontrolledAccardion)
+  const ControlledAccardionMemo = React.memo(ControlledAccardion)
+  const SelectMemo = React.memo(Select)
+
   return (
     <div className="App">
-      <UncontrolledAccardion setCollapsed={setCollapsed} collapsed={collapsed} items={items} />
-      <UncontrolRating setRatingValue={setRatingValue} ratingValue={ratingValue} stars={Stars}/>
-      <ControlRating />
-      <ControlledAccardion />
+      <UncontrolledAccardionMemo setCollapsed={setCollapsed} collapsed={collapsed} items={items} />
+      <UncontrolRatingMemo setRatingValue={setRatingValue} ratingValue={ratingValue} stars={Stars} />
+      <ControlRatingMemo />
+      <ControlledAccardionMemo />
+      <SelectMemo value={value} items={itemsSelected} onChange={setValue} />
     </div>
   );
 }
