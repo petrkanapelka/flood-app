96afa4062502e5bf19075fd357da73d48cbb5162
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-29 23:10:39 +0300

    storybook: create Clock story

---
+++ src/components/clock/Clock.stories.tsx
@@ -0,0 +1,14 @@
+import { Clock } from "./Clock";
+
+
+export default {
+    title: 'Clock',
+    component: Clock
+}
+
+
+export const BaseExample = () => {
+    return (
+        <Clock type='analog' />
+    )
+}
